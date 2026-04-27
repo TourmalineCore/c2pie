6ee116f0a494a2d0100b56b8b1646bf139ccc625
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2026-04-27 11:02:21 +0500

    fix: #55: convert line endings from CRLF to LF

--- c2pie/signing.py
+++ c2pie/signing.py
@@ -209,7 +209,7 @@ def sign_file(
         cai_offset=cai_offset, hashed_data=hashlib.sha256(raw_bytes).digest()
     )
 
-    # This section should be replaced with the content generation logic once the relevant 
+    # This section should be replaced with the content generation logic once the relevant
     # functionality is available (example, action 'c2pa.opened' for Ingredient Assertion)
     actions_assertion = c2pie_GenerateActionsAssertion(action="c2pa.created")
 
