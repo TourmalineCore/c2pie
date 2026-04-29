6a23ffb3d4e9174613c4f21f2068a9730ae802cf
Oleg Kl <oklyavlin@tourmalinecore.com>
2026-04-29 18:45:00 +0500

    feat: #46: implement box parsing from bytes and base box class

--- c2pie/jumbf_boxes/box.py
+++ c2pie/jumbf_boxes/box.py
@@ -1,5 +1,11 @@
 # Base jumbf box class
 
+from c2pie.jumbf_boxes.constants import (
+    BYTE_ORDER,
+    HEADER_SIZE,
+    LBOX_SIZE,
+)
+
 
 class Box:
     def __init__(
@@ -26,3 +32,34 @@ class Box:
         t_box = bytes.fromhex(self.t_box)
         l_box = self.l_box.to_bytes(4, "big")
         return l_box + t_box + self.payload
+
+    @classmethod
+    def parse_from_bytes(cls, data: bytes, offset: int = 0) -> tuple["Box", int]:
+        if offset + HEADER_SIZE > len(data):
+            raise ValueError("Not enough data for box header")
+
+        l_box = int.from_bytes(
+            data[offset : offset + LBOX_SIZE],
+            BYTE_ORDER,
+        )
+        t_box = data[offset + LBOX_SIZE : offset + HEADER_SIZE].hex()
+
+        if l_box == 0:
+            end = len(data)
+        else:
+            end = offset + l_box
+
+        if end > len(data):
+            raise ValueError("Box length exceeds available data")
+
+        payload = data[offset + HEADER_SIZE : end]
+        box = cls(t_box, payload)
+        box.l_box = l_box
+        return box, end
+
+
+def iter_boxes(data: bytes):
+    offset = 0
+    while offset < len(data):
+        box, offset = Box.parse_from_bytes(data, offset)
+        yield box
--- c2pie/jumbf_boxes/super_box.py
+++ c2pie/jumbf_boxes/super_box.py
@@ -1,7 +1,8 @@
 # Jumbf super box class
 from __future__ import annotations
 
-from c2pie.jumbf_boxes.box import Box
+from c2pie.jumbf_boxes.box import Box, iter_boxes
+from c2pie.jumbf_boxes.constants import JUMB_TYPE
 from c2pie.jumbf_boxes.content_box import ContentBox
 from c2pie.jumbf_boxes.description_box import DescriptionBox
 from c2pie.utils.content_types import jumbf_content_types
@@ -19,6 +20,29 @@ class SuperBox(Box):
 
         payload = self.description_box.serialize() + self.serialize_content_boxes()
         super().__init__(b"jumb".hex(), payload=payload)
+
+    @classmethod
+    def from_box(cls, box: Box) -> SuperBox:
+        if box.get_type() != JUMB_TYPE:
+            raise ValueError("Box is not a JUMBF superbox")
+
+        children = list(iter_boxes(box.get_payload()))
+        if not children:
+            raise ValueError("Empty JUMBF superbox")
+
+        description_box = DescriptionBox.from_box(children[0])
+        content_boxes = children[1:]
+
+        instance = cls.__new__(cls)
+        instance.t_box = box.get_type()
+        instance.l_box = box.get_length()
+        instance.payload = box.get_payload()
+        instance.description_box = description_box
+        instance.content_boxes = content_boxes
+        return instance
+
+    def _serialize_children(self) -> bytes:
+        return b"".join(cb.serialize() for cb in self.content_boxes if cb is not None)
 
     def add_content_box(
         self,
