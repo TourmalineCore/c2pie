94444a3b86244722ceddb486e8aa0e8c7c8b48f5
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2026-05-04 13:46:56 +0500

    refactor(actions-assertion): #55: move Actions assertion tests to separate test file so that to split the tests among yourselves

---
+++ tests/c2pa/assertions/actions_assertion_test.py
@@ -0,0 +1,56 @@
+from c2pie.c2pa.assertion import ActionsAssertion
+from c2pie.utils.assertion_schemas import cbor_to_bytes
+from c2pie.utils.content_types import jumbf_content_types
+
+
+def test_actions_assertion_content_type_is_cbor():
+    actions_assertion = ActionsAssertion(action="c2pa.created")
+    assert actions_assertion.get_content_type() == jumbf_content_types["cbor"]
+
+
+def test_actions_assertion_label():
+    actions_assertion = ActionsAssertion(action="c2pa.created")
+    assert actions_assertion.get_label() == "c2pa.actions.v2"
+
+
+def test_actions_assertion_schema_fields():
+    actions_assertion = ActionsAssertion(action="c2pa.created")
+    assert actions_assertion.schema["actions"][0]["action"] == "c2pa.created"
+
+
+def test_actions_assertion_schema_has_no_parameters_by_default():
+    actions_assertion = ActionsAssertion(action="c2pa.created")
+    assert "parameters" not in actions_assertion.schema["actions"][0]
+
+
+def test_actions_assertion_schema_fields_with_parameters():
+    expected_parameters = {
+        "ingredients": [
+            {
+                "url": "self#jumbf=c2pa.assertions/c2pa.ingredient.v3",
+                "alg": "sha256",
+                "hash": "B-X0kqzQsdIOC9ZxU7M/FRaaLlQ8Ap1U95//TucEjkT=",
+            }
+        ]
+    }
+
+    actions_assertion = ActionsAssertion(action="c2pa.opened", parameters=expected_parameters)
+    assert actions_assertion.schema["actions"][0]["action"] == "c2pa.opened"
+    assert actions_assertion.schema["actions"][0]["parameters"] == expected_parameters
+
+
+def test_actions_assertion_serializes_as_cbor():
+    actions_assertion = ActionsAssertion(action="c2pa.created")
+
+    expected_payload = cbor_to_bytes(
+        {
+            "actions": [
+                {
+                    "action": "c2pa.created",
+                },
+            ],
+        },
+    )
+
+    assert len(actions_assertion.content_boxes) == 1
+    assert actions_assertion.content_boxes[0].payload == expected_payload
