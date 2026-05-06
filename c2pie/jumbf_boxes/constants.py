fe0f8c03799fca7ab573fabb789cde334fcb5beb
Oleg Kl <oklyavlin@tourmalinecore.com>
2026-05-06 22:38:31 +0500

    refactor: #46: run ruff format

--- c2pie/jumbf_boxes/constants.py
+++ c2pie/jumbf_boxes/constants.py
@@ -1,11 +1,11 @@
 # === JUMBF Box Header ===
-LBOX_SIZE = 4           # Length of the LBox field in bytes
-TBOX_SIZE = 4           # Length of the TBox field in bytes
+LBOX_SIZE = 4  # Length of the LBox field in bytes
+TBOX_SIZE = 4  # Length of the TBox field in bytes
 HEADER_SIZE = LBOX_SIZE + TBOX_SIZE  # Total box header size in bytes
 
 # === JUMBF Description Box (jumd) ===
 CONTENT_TYPE_SIZE = 16  # UUID content type size
-TOGGLE_SIZE = 1         # Description box flags size
+TOGGLE_SIZE = 1  # Description box flags size
 JUMD_MIN_PAYLOAD_SIZE = CONTENT_TYPE_SIZE + TOGGLE_SIZE + 1  # +1 for minimum null-terminator
 LABEL_OFFSET = CONTENT_TYPE_SIZE + TOGGLE_SIZE  # Label offset within the jumd payload
 
