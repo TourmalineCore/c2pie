3e92de2dc68f383e05b5636ebc74da909bed268d
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2026-05-15 09:59:25 +0500

    refactor: #47: refactor Manifest Store tests

--- tests/c2pa/manifest_store_test.py
+++ tests/c2pa/manifest_store_test.py
@@ -1,12 +1,14 @@
 from c2pie.c2pa.manifest import Manifest
 from c2pie.c2pa.manifest_store import ManifestStore
-from c2pie.jumbf_boxes.box import Box
 from c2pie.utils.content_types import c2pa_content_types
+
+
+def _previous_manifest_to_bytes(manifest_label: str) -> bytes:
+    return Manifest(manifest_label=manifest_label).serialize()
 
 
 def test_create_manifest_store_witn_no_content():
     test_manifest_store = ManifestStore()
-
     assert test_manifest_store is not None
     assert test_manifest_store.get_content_type() == c2pa_content_types["manifest_store"]
     assert test_manifest_store.get_label() == "c2pa"
@@ -14,69 +16,66 @@ def test_create_manifest_store_witn_no_content():
     assert len(test_manifest_store.content_boxes) == 0
 
 
-def test_create_manifest_store_with_manifests():
-    test_manifests = [Manifest(), Manifest()]
-
-    test_manifest_store = ManifestStore(manifests=test_manifests)
-
-    assert len(test_manifest_store.manifests) != 0
-    assert len(test_manifest_store.content_boxes) != 0
+def test_create_manifest_store_with_manifest():
+    manifest_store = ManifestStore(manifests=[Manifest()])
+    assert len(manifest_store.manifests) == 1
+    assert len(manifest_store.content_boxes) == 1
 
 
-def _prior_manifest_bytes(label: str) -> bytes:
-    return Manifest(manifest_label=label).serialize()
+def test_manifest_store_with_previous_manifests_content_boxes_count():
+    previous_manifests = [
+        _previous_manifest_to_bytes("urn:c2pa:previous-manifest1"),
+        _previous_manifest_to_bytes("urn:c2pa:previous-manifest2"),
+    ]
+
+    manifest = Manifest(manifest_label="urn:c2pa:new-manifest")
+
+    manifest_store = ManifestStore(
+        [
+            *previous_manifests,
+            manifest,
+        ],
+    )
+
+    assert len(manifest_store.manifests) == 3
+    assert len(manifest_store.content_boxes) == 3
 
 
-def test_manifest_store_with_prior_manifests_content_boxes_count():
-    prior = [_prior_manifest_bytes("urn:c2pa:old1"), _prior_manifest_bytes("urn:c2pa:old2")]
-    new_manifest = Manifest(manifest_label="urn:c2pa:new")
-    store = ManifestStore([*prior, new_manifest])
-    # 2 prior boxes + 1 new manifest
-    assert len(store.content_boxes) == 3
+def test_manifest_store_with_previous_manifests_new_manifest_last():
+    previous_manifests = [
+        _previous_manifest_to_bytes("urn:c2pa:previous-manifest"),
+    ]
+
+    manifest = Manifest(manifest_label="urn:c2pa:new-manifest")
+
+    manifest_store = ManifestStore(
+        [
+            *previous_manifests,
+            manifest,
+        ],
+    )
+
+    assert manifest_store.content_boxes[-1] is manifest
 
 
-def test_manifest_store_with_prior_manifests_new_manifest_last():
-    prior = [_prior_manifest_bytes("urn:c2pa:old")]
-    new_manifest = Manifest(manifest_label="urn:c2pa:new")
-    store = ManifestStore([*prior, new_manifest])
-    assert store.content_boxes[-1] is new_manifest
-
-
-def test_manifest_store_without_prior_manifests_unaffected():
-    new_manifest = Manifest(manifest_label="urn:c2pa:only")
-    store = ManifestStore([new_manifest])
-    assert len(store.content_boxes) == 1
-
-
-def test_manifest_store_only_manifest_objects_tracked_in_manifests():
-    prior = _prior_manifest_bytes("urn:c2pa:old")
-    new_manifest = Manifest(manifest_label="urn:c2pa:new")
-    store = ManifestStore([prior, new_manifest])
-    assert store.manifests == [new_manifest]
-
-
-def test_manifest_store_with_prior_manifests_serializes_without_error():
-    prior = _prior_manifest_bytes("urn:c2pa:prior")
-    store = ManifestStore([prior, Manifest()])
-    result = store.serialize()
-    assert isinstance(result, bytes)
-    assert len(result) > 0
+def test_manifest_store_with_previous_manifests_serializes_without_error():
+    previous_manifest = _previous_manifest_to_bytes("urn:c2pa:previous-manifest")
+    manifest_store = ManifestStore([previous_manifest, Manifest()])
+    manifest_store_bytes = manifest_store.serialize()
+    assert isinstance(manifest_store_bytes, bytes)
+    assert len(manifest_store_bytes) > 0
 
 
 def test_manifest_store_set_hash_data_length_only_affects_new_manifests():
-    prior_bytes = _prior_manifest_bytes("urn:c2pa:prior")
-
-    new_manifest = Manifest(manifest_label="urn:c2pa:new")
-    store = ManifestStore([prior_bytes, new_manifest])
-    # set_hash_data_length_for_all should not raise, prior boxes are not touched
-    store.set_hash_data_length_for_all(1024)
-
-    # Prior manifest raw bytes are unchanged (they're stored as parsed Box objects)
-    prior_box = store.content_boxes[0]
-    assert prior_box.serialize() == prior_bytes
+    previous_manifest = _previous_manifest_to_bytes("urn:c2pa:previous-manifest")
+    new_manifest = Manifest(manifest_label="urn:c2pa:new-manifest")
+    manifest_store = ManifestStore([previous_manifest, new_manifest])
+    manifest_store.set_hash_data_length_for_all(1024)
+    previous_box = manifest_store.content_boxes[0]
+    assert previous_box.serialize() == previous_manifest
 
 
 def test_manifest_store_empty_list_equivalent_to_no_args():
-    store = ManifestStore([])
-    assert len(store.manifests) == 0
-    assert len(store.content_boxes) == 0
+    manifest_store = ManifestStore([])
+    assert len(manifest_store.manifests) == 0
+    assert len(manifest_store.content_boxes) == 0
