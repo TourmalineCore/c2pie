e3152fb9aa7e86615514a08b6872ab13444aff8c
Artem Sheptunov <106321977+Infindery@users.noreply.github.com>
2026-05-21 16:57:58 +0500

    test: #82: add an end-to-end stress test; update schemas

--- tests/c2pa/e2e_test.py
+++ tests/c2pa/e2e_test.py
@@ -35,6 +35,24 @@ def _has_c2patool() -> bool:
     return shutil.which("c2patool") is not None
 
 
+def _sign_file_with_mock_uuids(
+    data_file: Path,
+    output_file: Path,
+):
+    fixed_uuid = uuid.UUID("47affab1a5d24c75b991fbc030e02448")
+
+    # If the call is not intercepted and a fixed value
+    # is not specified, a random value will be generated
+    with (
+        patch("c2pie.interface.uuid.uuid4", return_value=fixed_uuid),
+        patch("c2pie.c2pa.claim.uuid.uuid4", return_value=fixed_uuid),
+    ):
+        sign_file(
+            input_path=data_file,
+            output_path=output_file,
+        )
+
+
 def _validate_using_c2patool_and_return_json_report(asset_path: Path) -> dict:
     """
     Return c2patool's JSON report. If parsing fails, raise with stdout/stderr for debugging.
@@ -75,23 +93,37 @@ def test_e2e_signing_with_c2patool_validation(
     if not _has_c2patool():
         pytest.skip("c2patool not available")
 
-    output_file = tmp_path / f"out.{data_file.suffix}"
+    output_file = tmp_path / f"out{data_file.suffix}"
 
-    fixed_uuid = uuid.UUID("47affab1a5d24c75b991fbc030e02448")
-
-    # If the call is not intercepted and a fixed value
-    # is not specified, a random value will be generated
-    with (
-        patch("c2pie.interface.uuid.uuid4", return_value=fixed_uuid),
-        patch("c2pie.c2pa.claim.uuid.uuid4", return_value=fixed_uuid),
-    ):
-        sign_file(
-            input_path=data_file,
-            output_path=output_file,
-        )
+    _sign_file_with_mock_uuids(
+        data_file,
+        output_file,
+    )
 
     report = _validate_using_c2patool_and_return_json_report(output_file)
     manifests = report.get("manifests")
     expected_schema = json.loads(schema_file.read_text())
 
     assert manifests == expected_schema
+
+
+# @pytest.mark.parametrize(
+#     "iteration",
+#     range(100),
+# )
+# def test_e2e_signature_stability(iteration, tmp_path):
+#     if not _has_c2patool():
+#         pytest.skip("c2patool not available")
+
+#     data_file = Path(FILES_DIR / "test_image.jpg")
+#     output_file = tmp_path / f"out{data_file.suffix}"
+
+#     _sign_file_with_mock_uuids(
+#         data_file,
+#         output_file,
+#     )
+
+#     report = _validate_using_c2patool_and_return_json_report(output_file)
+#     validation_state = report.get("validation_state")
+
+#     assert validation_state == "Valid"
