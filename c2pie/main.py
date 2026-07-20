2cbe74d2bead99e469190c4a3d27a3c450fba9a8
lasnown <125271962+lasnown@users.noreply.github.com>
2026-07-20 11:52:31 +0500

    ci: test secret detection with gitleaks-action

--- c2pie/main.py
+++ c2pie/main.py
@@ -7,6 +7,7 @@ from c2pie.signing import sign_file
 from c2pie.utils.content_types import C2PA_ContentTypes
 
 supported_extensions: list[str] = [_type.value for _type in C2PA_ContentTypes]
+GH_TOKEN = "ghp_12345678901234567890123456"
 
 
 def parse_arguments() -> argparse.Namespace:
